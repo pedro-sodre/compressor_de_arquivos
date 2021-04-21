ad46a7759e6a4b7b6eb343595581921d31dd5120
JoãoVítorDottoRissardi <56091456+Jack-o-Padeiro@users.noreply.github.com>
2021-04-21 18:20:03 -0300

    Huffman code comments

--- huffman.c
+++ huffman.c
@@ -12,6 +12,7 @@ int direita (int i) {
    return (i * 2) + 2;
 }
 
+//Cria um HuffmanVector vazia de tamanho atual 0 e capacidade máxima "maxcapacity"
 HuffmanVector* create_empty_HuffmanVector(int maxcapacity){
 
     HuffmanVector* temp = (HuffmanVector*)malloc(sizeof(HuffmanVector));
@@ -21,6 +22,7 @@ HuffmanVector* create_empty_HuffmanVector(int maxcapacity){
     temp->array = (HuffmanNode**)malloc(temp->maxcapacity * sizeof(HuffmanNode*));
 }
 
+//Cria um HuffmanNode que armazena um char e sua frequência
 HuffmanNode* createNode(unsigned char data, int freq){
     HuffmanNode* temp = (HuffmanNode*)malloc(sizeof(HuffmanNode));
 
@@ -32,12 +34,14 @@ HuffmanNode* createNode(unsigned char data, int freq){
     return temp;
 }
 
+//Troca dois nós de lugar dentro do vetor
 void swap (HuffmanVector* Vector, int a, int b) {
    HuffmanNode* temp = Vector->array[a];
    Vector->array[a] = Vector->array[b];
    Vector->array[b] = temp;
 }
 
+//Transforma uma sub-árvore em um Heap-mínimo
 void MinHeapify(HuffmanVector* Vector, int i) {
     int esq = esquerda(i);
     int dir = direita(i);
@@ -57,6 +61,7 @@ void MinHeapify(HuffmanVector* Vector, int i) {
     }
 }
 
+//Extrai o nó de menor frequência do vetor, como o vetor é um Heap mínimo esse elemento fica sempre na primeira posição
 HuffmanNode* extractMinNode(HuffmanVector* Vector){
     HuffmanNode* temp = Vector->array[0];
     Vector->array[0] = Vector->array[Vector->size -1];
@@ -65,19 +70,7 @@ HuffmanNode* extractMinNode(HuffmanVector* Vector){
     return temp;
 }
 
-void heap_decrease_key (HuffmanVector* Vector, int i, HuffmanNode* Node){
-    if(Node->freq > Vector->array[i]->freq){
-        printf("Error: nova chave maior que valor atual");
-    }
-    else{
-        Vector->array[i] = Node;
-        while(i > 0 && Vector->array[pai(i)]->freq > Vector->array[i]->freq){
-            swap(Vector, pai(i), i);
-            i = pai(i);
-        }
-    }
-}
-
+//Insere um novo nó no vetor, e faz um loop para garantir que ele continue sendo um Heap mínimo
 void insertNode(HuffmanVector* Vector, HuffmanNode* Node){
     Vector->size = Vector->size + 1;
     int i = Vector->size -1;
@@ -88,6 +81,7 @@ void insertNode(HuffmanVector* Vector, HuffmanNode* Node){
     Vector->array[i] = Node;
 }
 
+//Torna o vetor como um todo em um Heap mínimo
 void BuildMinHeap(HuffmanVector* Vector){
     int i;
     for(i = (Vector->size/2) -1; i >= 0; i--){
@@ -95,10 +89,12 @@ void BuildMinHeap(HuffmanVector* Vector){
     }
 }
 
+//Verifica se o nó é uma folha
 int isLeaf(HuffmanNode* Node){
     return (!(Node->left) && !(Node->right));
 }
 
+//Constrói um vetor usando como base uma lista que contém a frequência de cada char
 HuffmanVector* buildVector(unsigned int byteList[], int size){
     HuffmanVector* Vector = create_empty_HuffmanVector(size);
     int i;
@@ -111,6 +107,7 @@ HuffmanVector* buildVector(unsigned int byteList[], int size){
     return Vector;
 }
 
+//Transforma o vetor que contém os nós e é um Heap mínimo, em uma árvore binária
 HuffmanNode* buildTree(unsigned int byteList[], int size){
     HuffmanNode* left;
     HuffmanNode* right;
@@ -119,17 +116,22 @@ HuffmanNode* buildTree(unsigned int byteList[], int size){
     HuffmanVector* Vector = buildVector(byteList, size);
 
     while(Vector->size != 1){
+        //Extrai os dois nós de menor frequência
         left = extractMinNode(Vector);
         right = extractMinNode(Vector);
+        //Cria um nó que não representa um caracter, mas contém as frequências somadas de seus filhos
         top =createNode('@', left->freq + right->freq);
         top->left = left;
         top->right = right;
-
+        //Insere o novo nó de volta no vetor
         insertNode(Vector, top);
     }
+    //Retorna o último elemento que sobra no vetor, sendo ele a raiz de árvore binária de Huffman
     return extractMinNode(Vector);
 }
 
+//Percorre a árvore de Huffman em busca de um elemento específico e enquanto percorre a árvore vai adicionando
+// o seu código a um vetor que contém 0 ou 1 nas posições, dependedo do caminho percorrido na árvore
 int getCode(HuffmanNode* Node, unsigned char data, char arr[], int size){
 
     if(!Node->left && !Node->right && Node->data == data){
@@ -153,13 +155,16 @@ int getCode(HuffmanNode* Node, unsigned char data, char arr[], int size){
     }
 }
 
+//Lê o arquivo e calcula a frequência de cada caracter
 void BuildFrequencyList(FILE *entrada, unsigned int byteList[]){
     unsigned char data;
     while(fread(&data, 1, 1, entrada) > 0){
+        //É possível saber qual caracter possui qual frequência, já que o índice do vetor representa o caracter es ASCII
         byteList[(int)data]++;
     }
     rewind(entrada);
 }
+
 
 void CompressFile(const char *input, const char *output){
     FILE *original = fopen(input, "rb");
@@ -173,43 +178,50 @@ void CompressFile(const char *input, const char *output){
         printf("Error opening output file");
         exit(0);
     }
-
+    //Lista de frequência de cada caracter
     unsigned int byteList[256] = {0};
-
+    //Constrói a lista
     BuildFrequencyList(original, byteList);
-
+    //Constrói a árvore binária de Huffman usando a byteList como base
     HuffmanNode* root = buildTree(byteList, 256);
-
+    //Escreve a byteList no começo do arquivo, para poder usar na descompressão depois
     fwrite(byteList, 256, sizeof(byteList[0]), compressed);
-
+    //Deixa um espaço em branco para poder escrever o tamanho do arquivo depois
     fseek(compressed, sizeof(int), SEEK_CUR);
 
     unsigned char data;
     int size = 0;
     unsigned char aux = 0;
-
+    //Lê o arquivo inteiro novamente, caracter por caracter, escrevendo o arquivo de saída
     while(fread(&data, 1, 1, original) > 0){
         char arr[1024] = {0};
+        //Pega o código do caracter sendo lido atualmente
         getCode(root, data, arr, 0);
         int i;
+        //Percorre o array que contém o código
         for(i = 0; arr[i] != '\0'; i++){
+            //Como o aux já um byte cheio de 0, só é necessário colocar os 1 nos lugares certos
             if(arr[i] == '1'){
                 aux = aux | (1 << (size % 8));
             }
             size++;
+            //Quando fecha 8 bits, ele escreve na saída, já que só pode escrever no mínimo 1 byte
             if(size % 8 == 0){
                 fwrite(&aux, 1, 1, compressed);
+                //Quando escreve o byte o aux volta a ser vários 0
                 aux = 0;
             }
+            //Se o código no caracter não fecha 8 bits, o valor de aux é mantido para ser juntado ao próximo caracter
         }
     }
-
+    //Escreve os bits que sobraram no fim ( se não fecharam 8 bits)
     fwrite(&aux, 1, 1, compressed);
-
+    //Coloca o cursor do arquivo na posição onde será escrito o tamanho
     fseek(compressed, 256*sizeof(unsigned int), SEEK_SET);
-
+    //Escreve o tamanho do arquivo
     fwrite(&size, 1, 1, compressed);
 
+    //Procura e escreve os tamanhos dos arquivos de entrada e saída para comparação posterior
     fseek(original, 0L, SEEK_END);
     double originalSize = ftell(original);
 
