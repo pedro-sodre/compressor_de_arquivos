7cd6b3e3e303f067cffdb8c35e3a3aaf3ed06735
Pedro Sodré <56206471+pedro-sodre@users.noreply.github.com>
2021-05-06 19:34:54 -0300

    Menu working properly

--- main_huffman.c
+++ main_huffman.c
@@ -4,11 +4,16 @@
 
 int main(){
     //menu
-    
+
     int escolha;
-    char arquivo[30];
+    const char* arquivo;
+    char* arquivo_saida;
+    arquivo = (const char*)malloc(sizeof(const char));
+    arquivo_saida = (char*)malloc(sizeof(const char));
+    int i;
+
     do {
-        printf("\nO que voc√™ deseja fazer?");
+        printf("\nO que vocÍ deseja fazer?");
         printf("\n1 - Comprimir Arquivo");
         printf("\n2 - Descomprimir Arquivo");
         printf("\n3 - Sair\n");
@@ -17,38 +22,72 @@ int main(){
 
         switch (escolha) {
         case 1:
-            printf("Qual o nome do arquivo ?\n");
+            printf("Qual o nome do arquivo que deseja comprimir ? (Nome do arquivo + formato) Ex: lorem_ipsum.txt\n");
             scanf("%s", arquivo);
+            strcpy(arquivo_saida, arquivo);
+            /*
+            arquivo_saida = strtok(arquivo_saida, ".");
+            printf("%s \n", arquivo);
+            printf("%s \n", arquivo_saida);
+            */
+            //Manipula a string pra tirar o tipo do arquivo (ex: tira o .txt)
+            for (i = strlen(arquivo_saida)-1; i>=0; i--){
+                if (arquivo_saida[i] == '.'){
+                    arquivo_saida[i] = '\0';
+                    break;
+                }
+
+            }
+            arquivo_saida = strcat(arquivo_saida, "-comprimido");
+
             printf("Deseja comprimir por 1 - Huffman ou por 2 - LZ78 ?\n");
             scanf("%d", &escolha);
             if (escolha == 1){
-                CompressFile(arquivo, srtcat(arquivo, "-comprimido"));
+                printf("\nComprimindo Huffman...\n");
+                CompressFile(arquivo, arquivo_saida);
+                printf("'%s' Comprimido com sucesso para '%s'!\n", arquivo, arquivo_saida);
             }else if (escolha == 2){
-                CompressFileLZ78Trie(arquivo, srtcat(arquivo, "-comprimido"));
+                printf("\nComprimindo LZ78...\n");
+                CompressFileLZ78Trie(arquivo, arquivo_saida);
+                printf(" '%s' Comprimido com sucesso para %s!\n", arquivo, arquivo_saida);
             }
             break;
         case 2:
-            printf("Qual o nome do arquivo ?\n");
+            printf("Qual o nome do arquivo que deseja descomprimir ? Ex: lorem_ipsum-comprimido\n");
             scanf("%s", arquivo);
+            strcpy(arquivo_saida, arquivo);
+
+            for (i = strlen(arquivo_saida)-1; i>=0; i--){
+                if (arquivo_saida[i] == '.'){
+                    arquivo_saida[i] = '\0';
+                    break;
+                }
+            }
+            arquivo_saida = strcat(arquivo_saida, "-descomprimido");
+
             printf("Deseja descomprimir por 1 - Huffman ou por 2 - LZ78 ?\n");
             scanf("%d", &escolha);
             if (escolha == 1){
-                DecompressFile(arquivo, strcat(arquivo, "-descomprimido"));
+                printf("\nDescomprimindo Huffman...\n");
+                DecompressFile(arquivo, arquivo_saida);
+                printf("'%s' Descomprimido com sucesso para '%s' !\n", arquivo, arquivo_saida);
             }else if (escolha == 2){
-                DecompressFileLZ78Trie(arquivo, strcat(arquivo, "-descomprimido"));
+                printf("\nDescomprimindo LZ78...\n");
+                DecompressFileLZ78Trie(arquivo, arquivo_saida);
+                printf("'%s' Descomprimido com sucesso para '%s'!\n", arquivo, arquivo_saida);
             }
             break;
         case 3:
             printf("Saindo");
             break;
         default:
-            printf("Erro! Insira algo v√°lido\n");
+            printf("Erro! Insira algo v·lido\n");
             }
     }while(escolha != 3);
 
     return 0;
 
-    
+
 
     // LZ78Trie* trie = createLZ78Trie();
     // insertTrie(trie, "abaabacba", 1);
@@ -63,7 +102,7 @@ int main(){
     DecompressFileLZ78Trie("comprimidoLZ78", "descomprimidoLZ78.txt");
     printf("Descomprimido com sucesso!\n");
 
-    // printf("\n*********************************************************\n\n");   
+    // printf("\n*********************************************************\n\n");
 
     // printf("Comprimindo duplo...\n");
     // CompressFile("comprimidoLZ78", "comprimidoDuplo");
